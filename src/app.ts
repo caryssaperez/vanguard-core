90d0261fd65849bd9c03c5c05112fb1af95603f6
Caryssa Perez <caryssaperez@gmail.com>
2018-10-01 21:09:01 -0400

    Add more types, configure app more

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
 import * as express from 'express';
 import { Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
+import * as graphqlHTTP from 'express-graphql';
+import cookieSession = require('cookie-session');
+import passport = require('passport');
+import schema from './graphql/schema';
 
 class App {
   constructor() {
@@ -13,11 +21,30 @@ class App {
 
   private config(): void {
     this.app.use(bodyParser.json());
+
+    this.app.use(
+      '/graphql',
+      graphqlHTTP({
+        schema,
+        graphiql: true,
+      }),
+    );
+
+    this.app.use(
+      cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [process.env.COOKIE_KEY],
+      }),
+    );
+
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
   }
 
   private routes(): void {
     const router = express.Router();
 
+    // Sample routes.
     router.get('/', (req: Request, res: Response) => {
       res.status(200).send({
         message: 'Hello World!',
@@ -26,7 +53,6 @@ class App {
 
     router.post('/', (req: Request, res: Response) => {
       const data = req.body;
-      // query a database and save data
       res.status(200).send(data);
     });
 
