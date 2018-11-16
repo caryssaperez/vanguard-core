5b397f0ee38cd25e4dbffcdc70c8483e581dca1f
Caryssa Perez <caryssaperez@gmail.com>
2018-11-16 18:27:01 -0500

    Configure Rspec

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,40 @@
+RSpec.configure do |config|
+  # See: http://tomdallimore.com/blog/taking-the-test-trash-out-with-databasecleaner-and-rspec/
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    unless Capybara.current_driver == :rack_test
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+end
