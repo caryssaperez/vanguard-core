0bad7ac1e1185c8e0a7aca0f7831416590f260b7
Caryssa Perez <caryssaperez@gmail.com>
2018-12-04 09:16:20 -0500

    Start user model

---
+++ app/models/user.rb
@@ -0,0 +1,69 @@
+class User < ApplicationRecord
+  has_many :arcs, dependent: :destroy
+
+  attr_accessor :remember_token, :password_reset_token
+
+  before_save :downcase_email
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness:  { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true,
+      length: { minimum: 6 },
+      allow_nil: true
+
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ?
+        BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns({ reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now })
+  end
+
+   # Returns true if a password reset has expired.
+   def password_reset_expired?
+    password_reset_at < 2.hours.ago
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+end
