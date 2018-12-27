c5d236fa9516512940822dd5094fedc0aaa2830e
Caryssa Perez <caryssaperez@gmail.com>
2018-12-27 15:53:43 -0500

    Use JWT gem

---
+++ app/models/auth_token.rb
@@ -0,0 +1,19 @@
+class AuthToken
+
+  JWT_ALGORITHM =  "HS256"
+
+  def self.payload(user)
+    {
+      exp: Time.now.to_i + 60 * 60,
+      iat: Time.now.to_i,
+      iss: ENV["JWT_ISSUER"],
+      user: {
+        email: user.email
+      }
+    }
+  end
+
+  def self.token
+    JWT.encode(payload, ENV["JWT_SECRET"], JWT_ALGORITHM)
+  end
+end
